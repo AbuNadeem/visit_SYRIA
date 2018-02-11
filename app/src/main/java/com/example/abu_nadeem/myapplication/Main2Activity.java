a758f15401f5846a1105a9dbcc27255885c5be25
batoolabuali <batool_abuali@yahoo.com>
2018-02-11 14:32:38 +0200

    Merge branch 'master' of https://github.com/mooody173/visit_SYRIA
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/abu_nadeem/myapplication/Main2Activity.java
    #	app/src/main/res/layout/activity_main2.xml

--- app/src/main/java/com/example/abu_nadeem/myapplication/Main2Activity.java
+++ app/src/main/java/com/example/abu_nadeem/myapplication/Main2Activity.java
@@ -1,7 +1,9 @@
 package com.example.abu_nadeem.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+
+    public void toAboutus(View view) {
+        Intent home = new Intent(Main2Activity.this, AboutusActivity.class);
+        startActivity(home, null);
     }
 }
